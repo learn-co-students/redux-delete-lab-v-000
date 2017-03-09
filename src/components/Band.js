52d0762fdc99fdf7fc632b23566843b7654d00d0
Cernan Bernardo <cernanb@gmail.com>
2017-03-09 07:59:38 -0800

    tests passing

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 class Band extends Component {
+  handleClick() {
+    const { id } = this.props
+    this.props.store.dispatch({
+      type: 'DELETE_BAND',
+      id
+    })
+  }
+
   render(){
     return(
       <div>
-        band component
+        <li>{this.props.text}</li>
+        <button onClick={() => this.handleClick()}>Delete</button>
       </div>
     )
   }
