31bb46a458207532bcc08246827513cee66293f1
BriOD <briodonoghue@gmail.com>
2017-06-11 11:50:42 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Band from './Band'
 
 class Bands extends Component {
   render() {
+    const bands = this.props.store.getState().bands.map((band, i)=> {
+      return <Band band={band} key={i} />
+    })
     return(
       <ul>
-        Bands Component
+        {bands}
       </ul>
     );
   }
