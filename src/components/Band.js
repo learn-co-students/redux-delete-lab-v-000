357265c554c305a0919e58c29da4e0640ed26c29
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-07-17 19:45:03 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+
+  handleClick = event => this.props.store.dispatch({type: "DELETE_BAND", id: this.props.band.id})
   render() {
     return(
       <div>
-        band component
+        <li>{this.props.band.text}</li>
+        <button type='button' onClick={this.handleClick}>Delete</button>
       </div>
     );
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,16 @@
+let count = 0
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return { bands: state.bands.concat(action.band) }
+    count++;
+      var band = Object.assign({}, action.band, {id: count})
+      return { bands: state.bands.concat(band) }
+
+      case 'DELETE_BAND':
+      return  {bands: state.bands.filter( band => band.id != action.id)}
     default:
       return state;
   }
