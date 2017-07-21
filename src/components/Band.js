a46bdd05e0230857f210199d7fd38a418dbd2c1c
DanL12186 <danlrcn@hotmail.com>
2017-07-21 14:06:29 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Band extends Component {
+
+  handleClick = () => {
+     this.props.store.dispatch({
+       id: this.props.band.id,
+       type: 'DELETE_BAND',
+     });
+  }
+
   render() {
-    return(
-      <div>
-        band component
-      </div>
+    return (
+      <li>{this.props.band.text}
+        <button onClick={this.handleClick}>Delete Band</button>
+      </li>
     );
   }
 };
 
 export default Band;
+
+
+//In the Band component, you will need to add a button that dispatches an action of type 'DELETE_BAND' and then passes through that band's id as the action.id.
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Band from './Band'
 
 class Bands extends Component {
   render() {
+    const bands = this.props.store.getState().bands.map((band, index) => <Band text={band.text} key={index} store={this.props.store} band={band}/>)
     return(
       <ul>
-        Bands Component
+        <div>
+          {bands}
+        </div>
       </ul>
     );
   }
