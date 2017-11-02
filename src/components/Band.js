3e086ed96a796795c87ba0f082583cf2abada617
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-11-02 14:11:22 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+
+  handleClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_BAND',
+      id: this.props.band.id,
+    })
+  }
+
   render() {
+    // console.log(this.props);
     return(
       <div>
-        band component
+        <li id={this.props.band.id}>
+          {this.props.band.text}
+          <button onClick={this.handleClick}>Delete band</button>
+        </li>
       </div>
     );
   }
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Band from './Band';
 
 class Bands extends Component {
   render() {
+    console.log(this.props.store.getState().bands);
+    const bands = this.props.store.getState().bands.map((band) =>{
+      return <Band band={band} store={this.props.store}/>
+    })
     return(
       <ul>
-        Bands Component
+        {bands}
       </ul>
     );
   }
