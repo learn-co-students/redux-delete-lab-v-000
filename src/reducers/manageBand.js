5de064c181aaa636957a40e790bf240f4454c5e7
LS-C <suzy.cho111@gmail.com>
2017-12-19 16:24:45 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,15 @@
+let id = 0;
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return { bands: state.bands.concat(action.band) }
+      id++
+      const bands = Object.assign({}, action.band, {id: id })
+      return { bands: state.bands.concat(bands) }
+    case 'DELETE_BAND':
+      const filteredBandList = state.bands.filter(band => band.id !== action.id)
+      return { bands: filteredBandList}
     default:
       return state;
   }
