1d78e00a3db0e69a99adaea31a1bb4577ccfedb3
Sophia Hubeny <shhubeny@gmail.com>
2017-12-22 17:55:38 +0000

    progress

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+
+  handleOnClick = () => {
+      this.props.store.dispatch({
+        type:'DELETE_BAND',
+        id: this.props.id
+      })
+  }
+
   render() {
     return(
-      <div>
-        band component
-      </div>
+        <li>
+          {this.props.text}
+          <button onClick={this.handleOnClick} />
+        </li>
     );
   }
 };
 
+Band.defaultProps = {
+  band:''
+}
 export default Band;
