f28a80d165df84527152e181b862d2089880a867
github username <github email address>
2018-02-26 00:08:56 +0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+
+  handleOnClick = (event) => {
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'DELETE_BAND',
+      id: this.props.band.id,
+    });
+  }
+
   render() {
-    return(
-      <div>
-        band component
-      </div>
+
+    return (
+      <li>
+        {this.props.band.text}<br></br>
+        <button onClick={this.handleOnClick.bind(this)}>Delete Band</button>
+      </li>
     );
   }
 };
