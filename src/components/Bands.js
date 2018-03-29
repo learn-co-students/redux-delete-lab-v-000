4ddde7c8255c9508d8e2c4fc579e1395131ee6e4
Hannah Carnes <hecarnes@gmail.com>
2018-03-29 10:44:34 -0400

    button dispatches DELETE_BAND when clicked

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render() {
+
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <li key={index} band={band}>{band.text}</li>
+    })
     return (
       <ul>
-        Bands Component
+        {bands}
       </ul>
     );
   }
 };
 
 export default Bands;
+
+
+
+// render() {
+
+//   const todos = this.props.store.getState().todos.map((todo, index) => {
+//     return <Todo text={todo.text} key={index} store={this.props.store} /* <- code change */ />
+//   });
+
+//   return (
+//     <ul>
+//       {todos}
+//     </ul>
+//   );
+// }
+// };
