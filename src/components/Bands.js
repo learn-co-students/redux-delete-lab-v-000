6d7c1ba0d30757e6a00da447eb550b6ebb16ceb4
Hannah Carnes <hecarnes@gmail.com>
2018-03-29 11:28:52 -0400

    import band component to display bands

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Band from './Band';
 
 class Bands extends Component {
   render() {
 
     const bands = this.props.store.getState().bands.map((band, index) => {
-      return <li key={index} band={band}>{band.text}</li>
+      return <Band key={index} band={band} store={this.props.store} />
     })
     return (
       <ul>
@@ -15,19 +16,3 @@ class Bands extends Component {
 };
 
 export default Bands;
-
-
-
-// render() {
-
-//   const todos = this.props.store.getState().todos.map((todo, index) => {
-//     return <Todo text={todo.text} key={index} store={this.props.store} /* <- code change */ />
-//   });
-
-//   return (
-//     <ul>
-//       {todos}
-//     </ul>
-//   );
-// }
-// };
