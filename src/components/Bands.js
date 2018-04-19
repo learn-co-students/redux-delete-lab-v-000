ec49d65bad8aace18fa580a368be1be49f44f66f
Joseph Surla <josephsurla@gmail.com>
2018-04-19 09:19:24 -0700

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Band from "./Band";
 
 class Bands extends Component {
   render() {
-    return(
-      <ul>
-        Bands Component
-      </ul>
-    );
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return (
+        <Band band={band} key={index} store={this.props.store}/>
+      );
+    });
+    return <ul>{bands}</ul>;
   }
-};
+}
 
 export default Bands;
