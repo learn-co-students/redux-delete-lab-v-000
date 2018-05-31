2426689be991cdad78f87933f2c96dce405d713b
Tavish Harrison <creationmargin26@gmail.com>
2018-05-31 00:40:33 +0000

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,16 @@
-export default function manageBand(state = {
-  bands: []
-}, action) {
-  switch (action.type) {
-    case 'ADD_BAND':
-      return { bands: state.bands.concat(action.band) }
-    default:
-      return state;
-  }
-};
+let id = 0;
+ export default function manageBand(state = {
+   bands: []
+ }, action) {
+   switch (action.type) {
+     case 'ADD_BAND':
+      id++
+      const band = Object.assign({}, action.band, {id: id});
+      return { bands: state.bands.concat(band) }
+    case 'DELETE_BAND':
+      const bands = state.bands.filter((b)=> b.id !== action.id)
+      return {bands}
+     default:
+       return state;
+   }
+ };
