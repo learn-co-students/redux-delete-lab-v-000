a4c94185790e008ef993a7236cf00c3361500af0
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-14 08:54:14 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -6,7 +6,7 @@ class Band extends Component {
     console.log(this)
     this.props.store.dispatch({
       type: 'DELETE_BAND',
-      id: this.props.id,
+      id: this.props.band.id,
     });
   }
 
