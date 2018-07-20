9bae17e4d715b6a4612391fb4a2f0724f2c45e4c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-07-20 16:52:45 -0700

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Band from './Band.js'
 
 class Bands extends Component {
   render() {
+    
+    let bands = this.props.store.getState().bands.map(band => <Band band={band} store={this.props.store}/>);
+
     return(
       <ul>
-        Bands Component
+        {bands}
       </ul>
     );
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,14 @@
+let id = 0;
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return { bands: state.bands.concat(action.band) }
+      id++
+      return { ...state, bands: state.bands.concat({ text: action.band.text, id: id }) }
+    case 'DELETE_BAND': 
+      return { ...state, bands: state.bands.filter(band => band.id !== action.id) }
     default:
       return state;
   }
