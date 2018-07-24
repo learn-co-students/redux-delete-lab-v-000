f3db4b8047adae8b6b57b96351f521eb346dac8b
Canino Developments <ian.canino@gmail.com>
2018-07-24 00:43:14 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Band extends Component {
-  render() {
-    return(
-      <div>
-        band component
-      </div>
-    );
+  handleClick (e) {
+    e.preventDefault()
+    this.props.store.dispatch({
+      type: 'DELETE_BAND',
+      id: this.props.band.id
+    })
   }
-};
-
-export default Band;
+  render () {
+    return (
+      <li>
+        {this.props.band.text}
+        <button onClick={(e) => this.handleClick(e)} />
+      </li>
+    )
+  }
+}
+export default Band
