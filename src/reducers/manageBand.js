bb506e7b993104a5a52b71f41ea5779922df9632
Michael Boroski <mjboroski@gmail.com>
2018-08-03 21:16:14 +0000

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,9 +3,11 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      const band = { id: Math.random(), name: action.name }
+      return { ...state, bands: [...state.bands, band] }
+    case 'DELETE_BAND':
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
     default:
       return state;
   }
