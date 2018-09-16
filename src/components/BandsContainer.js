ff0e7cf385da9b9bf03b0380b369eb9ca3bbc9b6
Christopher Harvey <chharvey@icloud.com>
2018-09-15 21:32:18 -0400

    Done.

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react'
-import BandInput from './BandInput';
-
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import BandInput from "./BandInput";
+import Band from "./Band";
+import { connect } from "react-redux";
 
 class BandsContainer extends Component {
+  displayBands = () =>
+    this.props.bands.map(band => (
+      <Band key={band.id} band={band} deleteBand={this.props.deleteBand} />
+    ));
+
   render() {
     return (
       <div>
-        <BandInput addBand={this.props.addBand}/>
-
+        <BandInput addBand={this.props.addBand} />
+        <ul>{this.displayBands()}</ul>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = ({ bands }) => ({ bands })
+const mapStateToProps = ({ bands }) => ({ bands });
 
 const mapDispatchToProps = dispatch => ({
-  addBand: name => dispatch({ type: "ADD_BAND", name })
-})
+  addBand: name => dispatch({ type: "ADD_BAND", name }),
+  deleteBand: id => dispatch({ type: "DELETE_BAND", id })
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BandsContainer);
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,12 +1,32 @@
-export default function manageBand(state = {
-  bands: []
-}, action) {
+export default function manageBand(
+  state = {
+    bands: []
+  },
+  action
+) {
   switch (action.type) {
-    case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+    case "ADD_BAND": {
+      return {
+        ...state,
+        bands: [
+          ...state.bands,
+          {
+            id:
+              state.bands.length === 0
+                ? 1
+                : state.bands[state.bands.length - 1].id + 1,
+            name: action.name
+          }
+        ]
+      };
+    }
+    case "DELETE_BAND": {
+      return {
+        ...state,
+        bands: state.bands.filter(band => band.id !== action.id)
+      };
+    }
     default:
       return state;
   }
-};
+}
