4f194ab1cf659e5e7dec83ef586d42d41812c708
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-11-03 03:38:29 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,134 +1,111 @@
 import { expect } from 'chai';
 import React from 'react';
-import { configure, shallow, mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import BandInput from '../src/components/BandInput';
 import sinon from 'sinon';
 import { renderer } from '../src/index';
-import { createStore } from 'redux'
-import { Provider, connect } from 'react-redux'
+import createStore from '../src/createStore';
 import manageBand from '../src/reducers/manageBand';
-import Band from '../src/components/Band'
-import App from '../src/App';
-import BandsContainer from '../src/components/BandsContainer';
-
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
+import { App } from '../src/App';
+import Bands from '../src/components/Bands';
+import Band from '../src/components/Band';
 
 describe('Bands Component', () => {
-  it('displays bands from Redux store', () => {
-    const store = createStore(manageBand)
+  it('displays a list of band components', () => {
+    const store = createStore(manageBand);
     sinon.stub(store, 'getState').returns({
       bands: [
-        { id: 1, text: 'The Legendary Pink Dots' },
-        { id: 2, text: 'The Castanets' },
-        { id: 3, text: 'Cool Runnings' }
+        { id: 1, text: 'hello' },
+        { id: 2, text: 'goodbye' }, 
+        { id: 3, text: 'ciao' }
       ]
     });
-
-    const wrapper = mount(<Provider store={store}><App /></Provider>)
-    expect(wrapper.find('li')).to.have.length(3);
+    const wrapper = shallow(<Bands store={store}/>);
+    expect(wrapper.find(Band)).to.have.length(3);
   });
 });
 
 describe('Band Component', () => {
-
-  it('displays the appropriate text on form submission', () => {
-    const store = createStore(manageBand)
-
-    const wrapper = mount(<Provider store={store}><App /></Provider>)
-    let input = wrapper.find('input').first()
-    let form = wrapper.find('form')
-
-    input.simulate('change', { target: { value: 'Gwar' } })
-    form.simulate('submit',  { preventDefault() {} })
-
-    expect(wrapper.text()).to.contain('Gwar');
+  it('displays the appropriate text', () => {
+    const band = { id: 1, text: 'hello' };
+    const wrapper = shallow(<Band band={band} />);
+    expect(wrapper.text()).to.contain('hello');
   });
 
   it('renders an li', () => {
-    const band = { id: 1, name: 'Momus' };
+    const band = { id: 1, text: 'hello' };
     const wrapper = shallow(<Band band={band} />);
     expect(wrapper.find('li')).to.have.length(1);
   });
 });
 
 describe('Redux', () => {
-
-  it('assigns a unique id property when a band is added', () => {
+  it('has an id property that increments with each element that is created', () => {
     const store = createStore(manageBand);
-    store.dispatch({type: 'ADD_BAND', name: 'Radiohead'})
-    store.dispatch({type: 'ADD_BAND', name: 'Devo'})
-    store.dispatch({type: 'ADD_BAND', name: 'Talking Heads'})
+    const wrapper = shallow(<BandInput store={store} />);
+    sinon.stub(renderer, "render");
+    let form = wrapper.find('form');
+    let input = wrapper.find('input').first();
+    input.simulate('change', { target: { value: 'Hello' } });
+    form.simulate('submit',  { preventDefault() {} });
 
-    let ids = store.getState()
-      .bands.map(band => band.id)
-      .filter((value, index, self) => {
-        return self.indexOf(value) === index;
-      })
-
-    expect(ids.length).to.equal(store.getState().bands.length)
-
+    expect(store.getState().bands[0].id).to.equal(1);
+    expect(store.getState().bands[0].text).to.equal('Hello');
+    input.simulate('change', { target: { value: 'ciao' } });
+    form.simulate('submit',  { preventDefault() {} });
+    expect(store.getState().bands[1].id).to.equal(2);
+    expect(store.getState().bands[1].text).to.equal('ciao');
   });
-
-  it('removes the correct band from the store on dispatch', () => {
-    const store = createStore(manageBand);
-    store.dispatch({type: 'ADD_BAND', name: 'Radiohead'})
-    store.dispatch({type: 'ADD_BAND', name: 'Devo'})
-    store.dispatch({type: 'ADD_BAND', name: 'Talking Heads'})
-
-    expect(store.getState().bands.length).to.equal(3)
-
-
-    let ids = store.getState()
-      .bands.map(band => band.id)
-
-    store.dispatch({type: 'DELETE_BAND', id: ids[1]})
-
-    let bandNames = store.getState().bands.map(band => band.name)
-
-    expect(store.getState().bands.length).to.equal(2)
-    expect(bandNames).to.not.include("Devo")
-  })
 });
 
 describe('Band Component with Redux', () => {
-
-  it('has a button that dispatches an DELETE_BAND action', ()=> {
+  it('has the band as props', () => {
     const store = createStore(manageBand);
-    store.dispatch({type: 'ADD_BAND', name: 'Daft Punk'})
-
-    const wrapper = mount(<Provider store={store}><App /></Provider>);
-
-    let deleteButton = wrapper.find('button').first();
-
-    expect(store.getState().bands.length).to.equal(1)
-    deleteButton.simulate('click',  { preventDefault() {} });
-    expect(store.getState().bands.length).to.equal(0)
-
+    sinon.stub(store, 'getState').returns({
+      bands: [
+        { id: 1, text: 'hello' },
+        { id: 2, text: 'goodbye' }, 
+        { id: 3, text: 'ciao' }
+      ]
+    });
+    const wrapper = shallow(<Bands store={store} />);
+    expect(wrapper.find({ band: { id: 1, text: 'hello' } })).to.have.length(1);
   });
 
+  it('has a button that dispatches a DELETE_BAND action when clicked', ()=> {
+    const store = createStore(manageBand);
+    const band = { id: 1, text: 'hello' };
+    const wrapper = shallow(<Band store={store} band={band} />);
+    let deleteButton = wrapper.find('button').first();
+    let stub = sinon.stub(store, "dispatch");
+    deleteButton.simulate('click',  { preventDefault() {} });
+    expect(stub.calledWith(sinon.match({ type: 'DELETE_BAND' }))).to.equal(true);
+  });
+
+  it('has a button that dispatches a DELETE_BAND action with the proper id when clicked', ()=> {
+    const store = createStore(manageBand);
+    const band = { id: 1, text: 'hello' };
+    const wrapper = shallow(<Band store={store} band={band} />);
+    let deleteButton = wrapper.find('button').first();
+    let stub = sinon.stub(store, "dispatch");
+    deleteButton.simulate('click',  { preventDefault() {} });
+    expect(stub.calledWith(sinon.match({ type: 'DELETE_BAND', id: 1 }))).to.equal(true);
+  });
 
   it('updates the state of the store to remove the component', () => {
     const store = createStore(manageBand);
-    const wrapper = mount(<Provider store={store}><App /></Provider>);
-
+    const wrapper = mount(<BandInput store={store} />);
     let form = wrapper.find('form');
     let input = wrapper.find('input').first();
-
-    input.simulate('change', { target: { value: 'Shannon and the Clams' } });
+    input.simulate('change', { target: { value: 'Hello' } });
     form.simulate('submit',  { preventDefault() {} });
-    input.simulate('change', { target: { value: 'Brian Eno' } });
+    input.simulate('change', { target: { value: 'ciao' } });
     form.simulate('submit',  { preventDefault() {} });
-
-    expect(store.getState().bands.length).to.equal(2)
-    expect(store.getState().bands[0].name).to.equal('Shannon and the Clams')
-    expect(store.getState().bands[1].name).to.equal('Brian Eno')
-
-    let deleteButton = wrapper.find('button').first();
+    let band = store.getState().bands[1];
+    const BandComponent = shallow(<Band store={store} band={band} />);
+    let deleteButton = BandComponent.find('button').first();
     deleteButton.simulate('click', { preventDefault() {} });
-
     expect(store.getState().bands.length).to.equal(1);
-    expect(store.getState().bands[0].name).to.equal('Brian Eno');
+    expect(store.getState().bands[0].text).to.equal('Hello');
   });
 });
