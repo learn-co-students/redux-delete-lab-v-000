3e11ea0f0252b555f5fba4f62ae6aa00129e345a
NeedsAUsername <kevinchi118@gmail.com>
2018-11-05 16:11:58 -0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -4,9 +4,10 @@ class Band extends Component {
 
   render() {
     return(
-      <div>
-        Band Component
-      </div>
+      <li>
+        {this.props.band.name}
+        <button onClick={() => this.props.deleteBand(this.props.band.id)}>Delete</button>
+      </li>
     );
   }
 };
