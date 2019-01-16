81056052b5e471cec0e232d45bf53a16b32e2e94
Michael Pope <popemray1986@gmail.com>
2019-01-16 17:41:52 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import manageBand from './reducers/manageBand'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const store = createStore(manageBand)
+const store = createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 ReactDOM.render(
