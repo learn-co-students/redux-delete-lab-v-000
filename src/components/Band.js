b80450c51153750fbe059425d4e0b30854ee3d4f
Vibraniumforge <gregm9999@gmail.com>
2019-02-16 17:16:20 -0800

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class Band extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   handleClick = () => {
     this.props.deleteBand(this.props.id);
   };
