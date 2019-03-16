e9e37ff1958e99b152e25d714d888e937b812721
terryblue99 <terryblue@comcast.net>
2019-03-16 18:45:47 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,10 +3,24 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+    	
+      // Generate a long random id number for each todo created
+      // and store an array of objects
+      const band = {
+      	id: Math.random()*10000000000000000,
+      	name: action.name
+      }
 
-      return { ...state, bands: [...state.bands, action.name] }
+      return action.name !== '' ? { bands: state.bands.concat(band) } : state
+
+    case 'DELETE_BAND':
+
+    	return {
+    		bands: state.bands.filter(band => band.id !== action.id)
+    	}
 
     default:
-      return state;
+
+      return state
   }
-};
+}
