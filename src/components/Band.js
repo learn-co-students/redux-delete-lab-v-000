a5f1a1c49e5ceea9f9a6dd90018b789bc88cffbd
“PetyaDjanovska” <petya.djanovska@gmail.com>
2019-03-22 14:59:27 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
-
   render() {
-    return(
-      <div>
-        Band Component
-      </div>
-    );
+    return (
+      <li>
+        <span>{this.props.bandName}</span>
+        <button onClick={() => this.props.delete(this.props.id)}>DELETE</button>
+      </li>
+    )
   }
-};
+}
 
 export default Band;
