f8f19e1b2aa7fb5b5498046c61774592f7ceb2fc
Steve Albonico <steve@albonico.net>
2019-04-08 13:20:24 -0700

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,8 +3,16 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+    const band = {
+                id: Math.random()*10000000000000000,
+                bandName: action.payload
+              }
+      return { bands: state.bands.concat(band) };
 
-      return { ...state, bands: [...state.bands, action.name] }
+      case 'DELETE_BAND':
+        
+        return { bands: state.bands.filter(band => band.id !== action.payload)}
+
 
     default:
       return state;
