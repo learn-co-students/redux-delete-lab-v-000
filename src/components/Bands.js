a6b04c40ca8697813627ea93faa16426348a9c31
amckean12 <amckean@me.com>
2019-04-28 12:57:13 -0500

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Band from './Band';
+
+class Bands extends Component {
+
+  renderBands = () => {
+    return this.props.bands.map(band => <Band {...band} key={band.id} deleteBand={this.props.deleteBand}/>)
+  }
+
+  render() {
+    return(
+      <div>
+        {this.renderBands()}
+      </div>
+    );
+  }
+};
+
+export default Bands;
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,9 +3,13 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+     const band = {id: Math.random()*10000000000000000, name: action.name}
+     return{...state, bands: [...state.bands, band]}
+    case 'DELETE_BAND':
+      // Create a new array of band object that doesnt include the band we are deleting
+      const bands = state.bands.filter(band => band.id !== action.id);
+      // the new state
+      return { bands };
     default:
       return state;
   }
