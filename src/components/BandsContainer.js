8a53c14ec68d88c753646a0e287f22eed0937f53
amitnyc83 <amit_nyc@live.com>
2019-05-02 17:41:27 -0400

    able to add delete button and delete band'

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import BandInput from './BandInput';
-
+import Band from './Band';
 import { connect } from 'react-redux'
 
 class BandsContainer extends Component {
+
+
+
   render() {
     return (
       <div>
         <BandInput addBand={this.props.addBand}/>
-
+        {this.props.bands.map(band => <Band band={band} key={band.id} delete={this.props.delete} />)}
       </div>
     )
   }
@@ -17,7 +20,8 @@ class BandsContainer extends Component {
 const mapStateToProps = ({ bands }) => ({ bands })
 
 const mapDispatchToProps = dispatch => ({
-  addBand: name => dispatch({ type: "ADD_BAND", name })
+  addBand: name => dispatch({ type: "ADD_BAND", name }),
+  delete: id => dispatch({type: "DELETE_BAND", id})
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
