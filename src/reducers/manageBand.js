925e74139fada29072e40d00bf65705a4162e42e
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-07-10 22:03:40 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,19 @@
 export default function manageBand(state = {
   bands: []
 }, action) {
+  console.log(action);
   switch (action.type) {
     case 'ADD_BAND':
 
-      return { ...state, bands: [...state.bands, action.name] }
+      const band = {
+        id: Math.random()*10000000000000000,
+        name: action.name
+      }
+
+      return { bands: state.bands.concat(band) };
+
+    case 'DELETE_BAND':
+      return {bands: state.bands.filter(band => band.id !== action.id)}
 
     default:
       return state;
