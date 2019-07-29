086d7434b6901da0173ce547795c36bd6f658e8d
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-07-28 20:47:42 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
-  render() {
-    return(
-      <div>
-        Band Component
-      </div>
-    );
+  handleOnClick = () => {
+  	this.props.deleteBand(this.props.band.id)
   }
-};
+
+  render() {
+  	return (
+  	  <div>
+  	    <li>{this.props.band.name}</li>
+  	    <button onClick={this.handleOnClick}>Delete Band</button>
+  	  </div>
+  	)
+  }
+}
 
 export default Band;
