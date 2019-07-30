5ee5da200f9d724ea4d287ddb8d791ef01116105
Rose Brennan <rose@grubhub.com>
2019-07-30 14:16:05 -0600

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -4,7 +4,16 @@ export default function manageBand(state = {
   switch (action.type) {
     case 'ADD_BAND':
 
-      return { ...state, bands: [...state.bands, action.name] }
+      const band = {
+        id: Math.random(),
+        name: action.name
+      }
+
+      return { bands: state.bands.concat(band) };
+
+    case 'DELETE_BAND':
+    console.log('inside delete: ', action.id)
+      return {bands: state.bands.filter(band => band.id !== action.id)}
 
     default:
       return state;
