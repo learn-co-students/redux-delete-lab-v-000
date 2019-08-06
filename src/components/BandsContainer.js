ed16ea1cc81cedf2b1ae243be35b013f0ca579db
sarawall1187 <sarawall1187@gmail.com>
2019-08-06 08:42:29 -0700

    Done.

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import BandInput from './BandInput';
+import Bands from './Bands'
 
 import { connect } from 'react-redux'
 
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,8 +3,12 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      const band = { id: Math.random(), name: action.name }
+      return { ...state, bands: [...state.bands, band] }
 
-      return { ...state, bands: [...state.bands, action.name] }
+    case 'DELETE_BAND':
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
 
     default:
       return state;
