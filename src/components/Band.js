90dbff0206ba5a911d92b22aed9d87a6ded4cd95
github username <github email address>
2019-10-25 10:25:45 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Band extends Component {
 
   render() {
+
+      let bands = this.props.bands.map(band => <li key={band.id}>{band.bandName}</li>);
+
     return(
       <div>
         Band Component
+        <ul>
+          {bands}
+        </ul>
       </div>
     );
   }
 };
 
-export default Band;
+const mapStateToProps = state => {
+  return { bands: state.bands }
+};
+
+export default connect(
+  mapStateToProps
+)(Band);
