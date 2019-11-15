368f7b63aae4076a1c0ad7d3e2322929f5bd0b4e
github username <github email address>
2019-11-14 20:52:36 -0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Band extends Component {
 
   render() {
+    debugger;
     return(
       <div>
-        Band Component
+        <span>{this.props.band.name}</span>
+        <button onClick={() => this.props.deleteBand(this.props.band.id)}>DELETE</button>
       </div>
     );
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,20 @@
+
 export default function manageBand(state = {
   bands: []
 }, action) {
+
   switch (action.type) {
     case 'ADD_BAND':
+        const band = {
+         id: Math.random(),
+         name: action.name
+       }
+      return { ...state, bands: [...state.bands, band] }
 
-      return { ...state, bands: [...state.bands, action.name] }
+    case 'DELETE_BAND':
+      debugger;
+        const bands = state.bands.filter(band => band.id !== action.id);
+        return {bands};
 
     default:
       return state;
