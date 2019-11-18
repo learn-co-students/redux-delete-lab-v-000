44ca8b09405d3913fd4b990db1f5fe90d89fa5d7
github username <github email address>
2019-11-17 21:46:13 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,12 +1,39 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
 
-      return { ...state, bands: [...state.bands, action.name] }
+      const band = {
+        id: uuid(),
+        name: action.name
+      }
+      return { bands: state.bands.concat(band) };
+  
+      case 'DELETE_BAND':
+  
+        return {bands: state.bands.filter(band => band.id !== action.id)}
 
     default:
       return state;
   }
 };
+  
+// export default function manageBand(state = {
+//   bands: []
+// }, action) {
+//   switch (action.type) {
+//     case 'ADD_BAND':
+//       const band = { id: Math.random(), name: action.name }
+//       return { ...state, bands: [...state.bands, band] }
+//     case 'DELETE_BAND':
+//       const bands = state.bands.filter(band => band.id !== action.id);
+//       return { bands };
+//     default:
+//       return state;
+//   }
+// };
+
+
