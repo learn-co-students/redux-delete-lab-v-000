e9417225a92a2505d3b420267956a49a42a92eda
Amanda Murphy <helloamandamurphy@gmail.com>
2020-01-07 11:13:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
-import manageBand from './reducers/manageBand'
+import manageBand from './reducers/manageBand';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
