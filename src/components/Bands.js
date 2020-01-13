ffc81b80b2415d0b0547e7868911d14f840e9e5b
Monica Marziano <mmarziano1@gmail.com>
2020-01-13 18:46:27 -0500

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Band from './Band'
+import { render } from 'enzyme';
+
+
+export default class Bands extends React.Component {
+    render() {
+        return (
+          <div>
+            <Band />
+          </div>
+        )
+      }
+    } 
