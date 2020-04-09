00dd1de86f13f7a84705a3d49c592a23c91dbbaf
giancarloz <gzabaleta48@gmail.com>
2020-04-09 10:26:59 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,35 +1,35 @@
-// import { expect } from 'chai';
-// import React from 'react';
-// import { configure, shallow, mount } from 'enzyme';
-// import BandInput from '../src/components/BandInput';
-// import sinon from 'sinon';
-// import { renderer } from '../src/index';
-// import { createStore } from 'redux'
-// import { Provider, connect } from 'react-redux'
-// import manageBand from '../src/reducers/manageBand';
-// import Band from '../src/components/Band'
-// import App from '../src/App';
-// import BandsContainer from '../src/components/BandsContainer';
+import { expect } from 'chai';
+import React from 'react';
+import { configure, shallow, mount } from 'enzyme';
+import BandInput from '../src/components/BandInput';
+import sinon from 'sinon';
+import { renderer } from '../src/index';
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+import manageBand from '../src/reducers/manageBand';
+import Band from '../src/components/Band'
+import App from '../src/App';
+import BandsContainer from '../src/components/BandsContainer';
 
-// import Adapter from 'enzyme-adapter-react-16'
+import Adapter from 'enzyme-adapter-react-16'
 
-// configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() })
 
-// describe('Bands Component', () => {
-//   it('displays bands from Redux store', () => {
-//     const store = createStore(manageBand)
-//     sinon.stub(store, 'getState').returns({
-//       bands: [
-//         { id: 1, text: 'The Legendary Pink Dots' },
-//         { id: 2, text: 'The Castanets' },
-//         { id: 3, text: 'Cool Runnings' }
-//       ]
-//     });
+describe('Bands Component', () => {
+  it('displays bands from Redux store', () => {
+    const store = createStore(manageBand)
+    sinon.stub(store, 'getState').returns({
+      bands: [
+        { id: 1, text: 'The Legendary Pink Dots' },
+        { id: 2, text: 'The Castanets' },
+        { id: 3, text: 'Cool Runnings' }
+      ]
+    });
 
-//     const wrapper = mount(<Provider store={store}><App /></Provider>)
-//     expect(wrapper.find('li')).to.have.length(3);
-//   });
-// });
+    const wrapper = mount(<Provider store={store}><App /></Provider>)
+    expect(wrapper.find('li')).to.have.length(3);
+  });
+});
 
 // describe('Band Component', () => {
 
