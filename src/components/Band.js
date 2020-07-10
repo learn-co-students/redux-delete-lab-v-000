2b92c40285698222f703eda7664dce66141bdecf
mat_anderson <matandersonlv@hotmail.com>
2020-07-10 16:26:01 -0700

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
+  handleOnClick = () => {
+    this.props.deleteBand(this.props.id)
+  }
+
   render() {
     return(
       <div>
-        Band Component
+        <li>{this.props.band.name}</li>
+        <button onClick={() => this.props.delete(this.props.band.id)}>Delete band</button>
       </div>
-    );
+    )
   }
 };
 
