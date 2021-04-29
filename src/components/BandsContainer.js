7ee08449d51d7a27af985ce50c11681005780bf9
github username <d24dust@hotmail.com>
2021-04-29 15:16:33 -0400

    Done.

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import BandInput from './BandInput';
 
 import { connect } from 'react-redux'
+import Bands from './Bands';
 
 class BandsContainer extends Component {
   render() {
     return (
       <div>
         <BandInput addBand={this.props.addBand}/>
-
+        <Bands bands={this.props.bands} deleteAction={this.props.deleteBand}/>
       </div>
     )
   }
@@ -17,7 +18,8 @@ class BandsContainer extends Component {
 const mapStateToProps = ({ bands }) => ({ bands })
 
 const mapDispatchToProps = dispatch => ({
-  addBand: name => dispatch({ type: "ADD_BAND", name })
+  addBand: name => dispatch({ type: "ADD_BAND", name }),
+  deleteBand: id=> dispatch({type: "DELETE_BAND", id: id})
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,13 @@
+import uuid from 'uuid'
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      const newBand = {id: uuid(),name: action.name}
+      return { ...state, bands: [...state.bands, newBand] }
+    case 'DELETE_BAND':
+      return {...state, bands: state.bands.filter(band=> band.id !== action.id)}
     default:
       return state;
   }
